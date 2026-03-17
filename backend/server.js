184200ecc85bee9e01d7b06ed14a2e118c92e4db
Abdrahman100 <hosniabdrahman60@gmail.com>
2026-03-17 15:58:45 +0100

    test three

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,7 @@ const upload = multer({
 // Register Student
 // ======================
 
-app.post("/api/register", upload.single("receipt"), async (req, res) => {
+app.post("/api/registrations", upload.single("receipt"), async (req, res) => {
 
     try {
 
