b1a71a36b25e3e00fb031cff9bdc1b379d6b15dd
Abdrahman100 <hosniabdrahman60@gmail.com>
2026-03-17 15:27:19 +0100

    test three

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 // ======================
 // Get Students
